f63d08b0953df66a7f2cf257bb43406c9db2a1c4
wntjd612 <wntjdwntjd612@gmail.com>
2023-08-16 02:31:50 +0900

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(5500, () => {
+    console.log('Server is running on port 5500');
 });
-    
