debdcf235033ff84e2ccefd5c0023de6021a809e
wntjd612 <wntjdwntjd612@gmail.com>
2023-08-16 02:20:58 +0900

    Changes to be committed: 	modified:   Talk_kbo.html 	new file:   server.js
    채팅 시스템 구현

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // 모든 클라이언트에 메시지 전송
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
