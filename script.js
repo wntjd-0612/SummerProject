8f601f74ae671118033ad489741b27a1dafd351c
wntjd612 <wntjdwntjd612@gmail.com>
2023-08-16 16:51:47 +0900

    Changes to be committed: 	modified:   main.html 	deleted:    profile_styl.css 	deleted:    profile.html 	modified:   script.js     new file:   talk_epl.html 	new file:   talk_laliga.html 	new file:   talk_ligue1.html 	new file:   talk_mlb.html
    -프로필 삭제
    -talk_page 추가

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function show_news(){
-    window.location.href= "news.html"
-}
-function show_profile(){
-    window.location.href= "profile.html"
-}
 
 function KBO_Talk(){
-    window.location.href="Talk_kbo.html"
+    window.location.href="talk_kbo.html"
+}
+function MLB_Talk(){
+    window.location.href="talk_mlb.html"
+}
+function EPL_Talk(){
+    window.location.href="talk_epl.html"
+}
+function LALIGA_Talk(){
+    window.location.href="talk_laliga.html"
+}
+function LIGUE1_Talk(){
+    window.location.href="talk_ligue1.html"
 }
